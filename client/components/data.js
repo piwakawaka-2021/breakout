97dafd7173886d33e3945a4374c8f09f5c597581
student <student@eda-bc-03.localdomain>
2021-04-09 10:36:30 +1200

    add paddle and collision with ball

--- client/components/data.js
+++ client/components/data.js
@@ -3,9 +3,17 @@ let ball = {
   y: 200,
   r: 10,
   xSpeed: 3,
-  ySpeed: 3
+  ySpeed: 3,
 }
 
+let paddle = {
+  moveRight: false,
+  moveLeft: false,
+  w: 30,
+  x: innerWidth / 2,
+  y: 450,
+}
 export default {
-  ball: ball
+  ball: ball,
+  paddle: paddle,
 }
