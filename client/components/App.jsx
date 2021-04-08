26f7c929b992fb28f134222c9307e33801d18ef8
jack-r-buckley <jackrbuckley@gmail.com>
2021-04-09 02:05:46 +1200

    add canvas and bouncing ball

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,32 @@
-import React, { useState, useEffect, Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import data from './data.js'
 
 const App = () => {
+  const ball = data.ball
   const gameWidth = 800
-  const gameHeight = document.documentElement.clientHeight
+  const gameHeight = 500
+  // const gameHeight = document.documentElement.clientHeight
+  const canvasRef = useRef(null)
+
   useEffect(() => {
-    let gameArea = document.getElementById('gameCanvas')
-    console.log(gameArea)
-  })
+    const render = () => {
+      const canvas = canvasRef.current
+      const c = canvas.getContext('2d')
+      c.clearRect(0, 0, canvas.width, canvas.height)
+      c.beginPath()
+      c.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI)
+      c.fill()
+      ball.x += ball.xSpeed
+      ball.y += ball.ySpeed
+      if (ball.x + ball.r >= canvas.width || ball.x - ball.r <= 0) ball.xSpeed = -ball.xSpeed
+      if (ball.y + ball.r >= canvas.height || ball.y - ball.r <= 0) ball.ySpeed = -ball.ySpeed
+      requestAnimationFrame(render)
+    }
+    render()
+  }, [])
 
   return (
-    <canvas className="gameCanvas" width={gameWidth} height={gameHeight}></canvas>
+    <canvas ref={canvasRef} className="gameCanvas" width={gameWidth} height={gameHeight}></canvas>
   )
 }
 
---
+++ client/components/data.js
@@ -0,0 +1,11 @@
+let ball = {
+  x: 400,
+  y: 200,
+  r: 10,
+  xSpeed: 3,
+  ySpeed: 3
+}
+
+export default {
+  ball: ball
+}
